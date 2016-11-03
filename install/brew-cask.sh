da869a48aa3f7a75292f3ccc349f88d9c9c52c73
杨丁苗 <yangdingmiao@meituan.com>
2016-11-03 18:23:42 +0800

    update

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -39,6 +39,7 @@ brew cask install spectacle
 brew cask install the-unarchiver
 brew cask install thunder
 brew cask install xmind
+brew cask install hyperswitch
 
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
